45fd20e7a58d7f729d77cd790dd1f0450670cbcd
AnanaMJ <ana.jalba13@gmail.com>
2016-02-06 17:40:03 +0000

    Remove block head

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -9,8 +9,7 @@ var router = express.Router();
 router.get('/', function (req, res) {
 	res.render('dashboard/index', {
 		_: {
-			activePage: 'dashboard',
-			title: null
+			activePage: 'dashboard'
 		}
 	});
 });
